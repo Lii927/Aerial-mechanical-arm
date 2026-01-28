36099b8ef376171dbcb99ca6549ba6dfc1163fe5
Lyeee <835447391@qq.com>
2026-01-27 22:42:01 -0800

    真正删除 build 和 devel

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lll/PX4_Fly/src
-# Build directory: /home/lll/PX4_Fly/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("flying_code")
--- build/flying_code/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lll/PX4_Fly/src/flying_code
-# Build directory: /home/lll/PX4_Fly/build/flying_code
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
